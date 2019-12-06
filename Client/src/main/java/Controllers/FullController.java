7069d6eff700d15dc73c2fbff59edd1877106dea
Tomasz Pach <byku52@gmail.com>
2019-12-06 16:08:36 +0100

    Add comments

--- Client/src/main/java/Controllers/FullController.java
+++ Client/src/main/java/Controllers/FullController.java
@@ -7,25 +7,60 @@ import java.util.List;
 
 public abstract class FullController {
 
+    /**
+     * Metoda wyswietlajaca wiadomosc bledu
+     *
+     * @param message wiadomosc bledu
+     */
     public abstract void error(String message);
 
+    /**
+     * Metoda informujaca czy udalo sie zalogowac
+     *
+     * @param success true jesli sie udalo, false nie udalo
+     */
     public void logIn(boolean success) {
 
     }
 
-
+    /**
+     * Metoda wyswietlajaca aktywne gry
+     *
+     * @param games dane aktywnych gier
+     * @see GameData
+     */
     public void loadActiveGames(List<GameData> games) {
 
     }
 
+    /**
+     * Metoda ktora informuje o dolaczeniu do gry
+     *
+     * @param data informacje o grze, gdzie Username to nazwa wlasciciela gry, czyli osoby o bialych pionkach.
+     * @see GameData
+     */
     public void joinGame(GameData data) {
 
     }
 
+    /**
+     * Metoda informujaca o wykonanym ruchu
+     *
+     * @param x  wspolrzedna x
+     * @param y  wspolrzdna y
+     * @param me true - ja, false - przeciwnik
+     */
     public void move(int x, int y, boolean me) {
 
     }
 
+    /**
+     * Metoda dodatkowych akcji w grze, czyli Surrender, Pass, Continue itd.
+     *
+     * @param actionType typ akcji
+     * @param me         true - ja, false - przeciwnik
+     * @see GameCommandType
+     */
     public void gameAction(GameCommandType actionType, boolean me) {
 
     }
