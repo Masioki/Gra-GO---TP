1ca9773e8a070acd5ea423cfdef08a5f4e8bca18
regin <cicheckiigor@gmail.com>
2019-12-14 11:29:54 +0100

    special suicide test

--- Server/src/test/java/Domain/Game/GameLogicTest.java
+++ Server/src/test/java/Domain/Game/GameLogicTest.java
@@ -45,6 +45,23 @@ public class GameLogicTest {
     @Test
     public void specialSuicide() throws Exception
     {
-        //TODO - napisz test
+        GameLogic gameLogic = new GameLogic(19);
+        gameLogic.placePawn(1, 0, true);
+        gameLogic.placePawn(0, 1, true);
+        gameLogic.placePawn(0,2,true);
+        gameLogic.placePawn(1,3,true);
+        gameLogic.placePawn(2,3,true);
+        gameLogic.placePawn(3,2,true);
+
+        //czarne pionki
+        gameLogic.placePawn(1,1,false);
+        gameLogic.placePawn(1,2,false);
+        gameLogic.placePawn(2,2,false);
+
+        boolean outcome = gameLogic.placePawn(2,1,true);
+        System.out.println(gameLogic.getBoard().get(new Point(1,1)) );
+        System.out.println(gameLogic.getBoard().get(new Point(2,1)) );
+        assertEquals(outcome, true);
+
     }
 }
